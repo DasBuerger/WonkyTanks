25bcbe3b05773cdcca8047d670fb1c6266343212
DasBuerger <vprisoner@gmail.com>
2017-09-30 11:44:09 -0500

    WonkyTanks Minimum Viable Product. Initial build.

---
+++ Assets/Standard Assets/2D/Scripts/BulletBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public float attackSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(projectile, transform.position, transform.rotation);
+            bullet.AddForce(new Vector2(attackSpeed,0));
+        }
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/ExitState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitState : MonoBehaviour {
+    void Update()
+    {
+        if (transform.position.x >= 10.77) Application.Quit();
+    }
+}
